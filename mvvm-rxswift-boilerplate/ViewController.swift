a484c7d85e971721e33c0554c279836e7943447f
Delfín Hernández Gómez <dhernandez@atsistemas.com>
2019-05-02 11:45:58 +0200

    Init repo

---
+++ mvvm-rxswift-boilerplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvm-rxswift-boilerplate
+//
+//  Created by Delfín Hernández Gómez on 02/05/2019.
+//  Copyright © 2019 Delfín Hernández Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
